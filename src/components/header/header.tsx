e00e9b374b3b3ec8ff256f13038e948af8542c45
Volodymyr Honcharuk <vhoncharuk@amcbridge.com>
2022-01-28 10:48:54 +0200

    initial commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css"
+const Header: React.FC = () => {
+    return(
+        <div className="header-container">
+            <div className="header-title">Ваша безопасность наша работа</div>
+            <div>Наш телефон: 380986543663</div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+    return(
+        <div>
+            Try to deploy
+        </div>
+    )
+}
+
+export default MainPage
