3f90ece474876ae8e0868d7c22da7a033b8410f5
Volodymyr Honcharuk <vhoncharuk@amcbridge.com>
2022-01-29 11:58:50 +0200

    add slider

---
+++ src/components/3DSlider/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+import One from "../../assets/images/sliderImages/one.jpg";
+import Two from "../../assets/images/sliderImages/two.jpg";
+import Three from "../../assets/images/sliderImages/three.jpg";
+import Four from "../../assets/images/sliderImages/four.jpg";
+import Five from "../../assets/images/sliderImages/five.jpg";
+const data = [
+  { key: 1, url: One },
+  { key: 2, url: Two },
+  { key: 3, url: Three },
+  { key: 4, url: Four },
+  { key: 5, url: Five },
+];
+class Carousel extends Component {
+  componentDidMount() {
+    const options = {
+      duration: 300,
+      onCycleTo: () => {
+        console.log("New Slide");
+      },
+    };
+    M.Carousel.init(this.Carousel, options);
+  }
+
+  renderThis = () => {
+    return data.map((i) => (
+      <a key={i.url} className="carousel-item">
+        <img src={i.url} />
+      </a>
+    ));
+  };
+
+  render() {
+    return (
+      <div
+        ref={(Carousel) => {
+          this.Carousel = Carousel;
+        }}
+        className="carousel"
+      >
+        {this.renderThis()}
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import Vigil from "../../assets/images/vigil.png";
+import Carousel from "../../components/3DSlider";
 
 const MainPage = () => {
+  useEffect(() => {
+    window.addEventListener("scroll", checkScroll);
+  }, []);
+
+  const [animation, setAnimation] = useState({
+    descriptionClass: "opacity",
+    imageClass: "opacity",
+  });
+
+  const checkScroll = (event: any) => {
+    if (window.scrollY > 150 && animation.descriptionClass === "opacity") {
+      setAnimation({
+        ...animation,
+        descriptionClass: "description-warn-animation",
+        imageClass: "vigil-image-animation",
+      });
+    }
+  };
+
   return (
-    <div >
-        <section className="main-image-container">
-            Hello
-        </section>
+    <div>
+      <section className="main-image-container">
+        <div className="main-section">
+          <div>
+            <div className="main-row-1">Защитите свои активы с помощью</div>
+            <div className="main-row-2">
+              Услуг удаленного наблюдения в реальном времени
+            </div>
+            <button className="main-row-3">
+              Борьба с преступностью сейчас
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="description-section">
+        <div className="description-title">
+          Камеры без слежения показывают только уже совершенные преступления
+        </div>
+        <div className="description-warn-section">
+          <div className={`description-warn ${animation.descriptionClass}`}>
+            Наша компания сочетает в себе высокотехнологичные камеры и передовое
+            программное обеспечение с видеонаблюдением в реальном времени,
+            осуществляемым виртуальным охранником, чтобы выявлять и
+            останавливать преступления по мере их совершения
+          </div>
+          <div className={`vigil-image ${animation.imageClass}`}>
+            <img src={Vigil} alt="" width={300} height={300} />
+          </div>
+        </div>
+      </section>
+
+      <Carousel />
     </div>
   );
 };
