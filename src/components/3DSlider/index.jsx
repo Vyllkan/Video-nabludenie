23c73f65313642ce52fc260f1bc3eeadcd6399db
Volodymyr Honcharuk <vhoncharuk@amcbridge.com>
2022-01-30 20:20:27 +0200

    try deploy 4

--- src/components/3DSlider/index.jsx
+++ src/components/3DSlider/index.jsx
@@ -14,31 +14,31 @@ const data = [
   { key: 5, url: Five },
 ];
 class Carousel extends Component {
-  // componentDidMount() {
-  //   const options = {
-  //     duration: 300,
-  //     onCycleTo: () => {
-  //       console.log("New Slide");
-  //     },
-  //   };
-  //   M.Carousel.init(this.Carousel, options);   
-  // }
+  componentDidMount() {
+    const options = {
+      duration: 300,
+      onCycleTo: () => {
+        console.log("New Slide");
+      },
+    };
+    M.Carousel.init(this.Carousel, options);   
+  }
 
-  // renderThis = () => {
-  //   return data.map((i) => (
-  //     <a key={i.url} className="carousel-item">
-  //       <img src={i.url} alt=""/>
-  //     </a>
-  //   ));
-  // };
+  renderThis = () => {
+    return data.map((i) => (
+      <div key={i.key} className="carousel-item">
+        <img src={i.url} alt=""/>
+      </div>
+    ));
+  };
 
   render() {
     return (
       <div
-        // ref={(Carousel) => {
-        //   this.Carousel = Carousel;
-        // }}
-        // className="carousel"
+        ref={(Carousel) => {
+          this.Carousel = Carousel;
+        }}
+        className="carousel"
       >
         {this.renderThis()}
       </div>
